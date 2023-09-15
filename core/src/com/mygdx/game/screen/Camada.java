7c566cde7d1faeef60fc70e3dc69679e82b23e39
Arthur020104 <arthur020104@gmail.com>
2023-09-15 17:47:05 -0300

    refatorando o codigo do jogo

--- core/src/com/mygdx/game/screen/Camada.java
+++ core/src/com/mygdx/game/screen/Camada.java
@@ -23,6 +23,82 @@ public class Camada
 		this.gridSnap = (int) (Gdx.graphics.getWidth() /this.imageSize);
 		this.objetosCamadas = new Texture[this.gridSnap][this.gridSnap];
 	}
+	public Boolean[] posAdjOcupadas(Player player)
+	{
+		 int[][] posAdj = player.getAdjacentPositions(this.getGridSnap());
+         Boolean[] posOcupadas = new Boolean[4];
+         
+         // Verifique as posições adjacentes ocupadas
+         
+         for (int h = 0; h < posAdj.length; h++) 
+         {
+             if (posAdj[h] != null && this.getTexture(posAdj[h][0], posAdj[h][1]) != null) {
+                 posOcupadas[h] = true;
+             } else 
+             {
+                 posOcupadas[h] = false;
+             }
+         }
+         return posOcupadas;
+	}
+	public void verificaBombasNaCamada(Player currentPlayer, Player players[],float delta)
+	{
+		Bomba bombas[] = currentPlayer.bombas;
+		  if (bombas != null) 
+          {
+              for (int h = 0; h < bombas.length; h++) {
+                  int[] pos = bombas[h].getPosicao();
+                  List<int[]> localExplosao = currentPlayer.updateBombasTime(delta, this.getGridSnap());
+                  
+                  if (localExplosao != null) 
+                  {
+                      // Processar explosões
+                      for (int j = 0; j < localExplosao.size(); j++) 
+                      {
+                          int[] explosionCoords = localExplosao.get(j);
+                          int x = explosionCoords[0];
+                          int y = explosionCoords[1];
+                          if(x ==-1 || y == -1)
+                          {
+                          	//j = (((int)j/3)+1) *3;
+                          	//j--;
+                          	continue;
+                          }
+                          //System.out.println(x + "," + y);
+                          System.out.println("j:"+j+" len>"+localExplosao.size());
+                          Texture player1Texture = players[0].getTexture();
+                          Texture player2Texture = players[1].getTexture();
+                          Texture layer1Texture = this.getTexture(x, y);
+                          if (layer1Texture != null && (layer1Texture.equals(player1Texture) || layer1Texture.equals(player2Texture))) 
+                          {
+                              if (layer1Texture.equals(player2Texture)) 
+                              {
+                                  players[1].recebeDano(1);
+                              } else 
+                              {
+                                  players[0].recebeDano(1);
+                              }
+                          } else 
+                          {
+                              this.setTexture(Bomba.getExplosaTexture(), x, y);
+                          }
+                        //if j % 3 != 0 then j = proximo mutiplo de 3 > j
+                          //0 1 2 -> 3 4 5-> 6 7 8->
+                          if (layer1Texture != null ) 
+                          {
+                          	//j+=3;
+                          	j = (((int)j/3)+1) *3;
+                          	j--;
+                          }
+                      }
+                      this.setTexture(null, pos[0], pos[1]);
+                  } else 
+                  {
+                      this.setTexture(Bomba.getBombaTexture(), pos[0], pos[1]);
+                  }
+              }
+          }
+	}
 	public void updateCamada(float delta) {
 	    List<int[]> lista = new ArrayList<>();
 	    for (int x = 0; x < this.gridSnap; x++) 
--- core/src/com/mygdx/game/screen/MainGame.java
+++ core/src/com/mygdx/game/screen/MainGame.java
@@ -26,46 +26,10 @@ public class MainGame implements Screen
 	public MainGame(Grid game)
 	{
 		gameOver =false;
-		for(int i = 0;i<camadas.length;i++)
-		{
-			camadas[i] = new Camada(20);
-		}
 		this.game = game;
 		//Camada de detalhes do mapa PLACEHOLDER
-		for(int i = 0;i<camadas[1].getGridSnap();i+=2)
-		{
-			for(int j = 0;j<camadas[1].getGridSnap();j+=2)
-			{
-				Random random = new Random();		       
-		        int indexi = random.nextInt(detailTextures.length);
-		        int indexii = random.nextInt(detailTextures[indexi].length);
-				camadas[1].setTexture(detailTextures[indexi][indexii], i, j);
-			}
-		}
 		
-		//gereando chao tile de grama
-		for(int i = 0;i<camadas[0].getGridSnap();i++)
-		{
-			for(int j = 0;j<camadas[0].getGridSnap();j++)
-			{
-				camadas[0].setTexture(chaoTexture, i, j);
-			}
-		}
-		//gerando areia abaixo do tile quebravel
-		for(int i = 1;i<camadas[0].getGridSnap();i+=2)
-		{
-			for(int j = 1;j<camadas[0].getGridSnap();j+=2)
-			{
-				camadas[0].setTexture(new Texture("chao.png"), i, j);
-			}
-		}
-		for(int i = 1;i<camadas[3].getGridSnap();i+=2)
-		{
-			for(int j = 1;j<camadas[3].getGridSnap();j+=2)
-			{
-				camadas[3].setTexture(new Texture("tile.jpg"), i, j);
-			}
-		}
+		this.generateCamadaTextures();
 		players = new Player[2];
 		players[0] = new Player(0, 0, new Texture("badlogic.jpg"));
 		players[1] = new Player2(camadas[3].getGridSnap()-2, camadas[3].getGridSnap()-1, new Texture("player2.png"));
@@ -92,79 +56,14 @@ public class MainGame implements Screen
 	        for (int i = 0; i < players.length; i++) 
 	        {
 	            int[] playersPos = players[i].getCurrentPos();
-	            int[][] posAdj = players[i].getAdjacentPositions(camadas[3].getGridSnap());
-	            Boolean[] posOcupadas = new Boolean[4];
-	            
-	            // Verifique as posições adjacentes
-	            for (int h = 0; h < posAdj.length; h++) 
-	            {
-	                if (posAdj[h] != null && camadas[3].getTexture(posAdj[h][0], posAdj[h][1]) != null) {
-	                    posOcupadas[h] = true;
-	                } else 
-	                {
-	                    posOcupadas[h] = false;
-	                }
-	            }
+	            Boolean[] posOcupadas = camadas[3].posAdjOcupadas(players[i]);
 	            
 	            players[i].handleInput(Gdx.input, camadas[3].getGridSnap(), posOcupadas);
 	            
 	            // Verifique as bombas na cena
-	            if (players[i].bombas != null) 
-	            {
-	                for (int h = 0; h < players[i].bombas.length; h++) {
-	                    int[] pos = players[i].bombas[h].getPosicao();
-	                    List<int[]> localExplosao = players[i].updateBombasTime(delta, camadas[3].getGridSnap());
-	                    
-	                    if (localExplosao != null) 
-	                    {
-	                        // Processar explosões
-	                        for (int j = 0; j < localExplosao.size(); j++) 
-	                        {
-	                            int[] explosionCoords = localExplosao.get(j);
-	                            int x = explosionCoords[0];
-	                            int y = explosionCoords[1];
-	                            if(x ==-1 || y == -1)
-	                            {
-	                            	//j = (((int)j/3)+1) *3;
-	                            	//j--;
-	                            	continue;
-	                            }
-	                            //System.out.println(x + "," + y);
-	                            System.out.println("j:"+j+" len>"+localExplosao.size());
-	                            Texture player1Texture = players[0].getTexture();
-	                            Texture player2Texture = players[1].getTexture();
-	                            Texture layer1Texture = camadas[3].getTexture(x, y);
-	                            if (layer1Texture != null && (layer1Texture.equals(player1Texture) || layer1Texture.equals(player2Texture))) 
-	                            {
-	                                if (layer1Texture.equals(player2Texture)) 
-	                                {
-	                                    players[1].recebeDano(1);
-	                                } else 
-	                                {
-	                                    players[0].recebeDano(1);
-	                                }
-	                            } else 
-	                            {
-	                                camadas[3].setTexture(Bomba.getExplosaTexture(), x, y);
-	                            }
-	                          //if j % 3 != 0 then j = proximo mutiplo de 3 > j
-	                            //0 1 2 -> 3 4 5-> 6 7 8->
-	                            if (layer1Texture != null ) 
-	                            {
-	                            	//j+=3;
-	                            	j = (((int)j/3)+1) *3;
-	                            	j--;
-	                            }
-	                        }
-	                        camadas[3].setTexture(null, pos[0], pos[1]);
-	                    } else 
-	                    {
-	                        camadas[3].setTexture(Bomba.getBombaTexture(), pos[0], pos[1]);
-	                    }
-	                }
-	            }
+	            camadas[3].verificaBombasNaCamada(players[i], players, delta);
 	            
-	            // Atualize a camada 1
+	            // Atualize a camada 3
 	            camadas[3].updateCamada(delta);
 	            
 	            // Atualize as posições dos jogadores na camada
@@ -174,35 +73,9 @@ public class MainGame implements Screen
 	            
 	            // Renderize as camadas
 	            game.batch.begin();
-	            for (int camada = 0; camada < camadas.length; camada++) 
-	            {
-	                for (int h = 0; h < camadas[camada].getGridSnap(); h++) 
-	                {
-	                    for (int j = 0; j < camadas[camada].getGridSnap(); j++) 
-	                    {
-	                        if (camadas[camada].getTexture(h, j) != null) 
-	                        {
-	                        	if(camada != 1)
-	                        	{
-	                        		game.batch.draw(camadas[camada].getTexture(h, j), h * camadas[camada].getImageSize(), j * camadas[camada].getImageSize(), camadas[camada].getImageSize(), camadas[camada].getImageSize());
-	                        	}
-	                        	else 
-	                        	{//Camada de detalhes do mapa PLACEHOLDER
-	                        		
-	                        		// Usando uma funcao hash para gerar numeros pseudo-aleatorios
-	                        		long seed = (long)h * 2654435761L + (long)j * 2654435789L;
-	                        		Random random = new Random(seed);
-	                        		//Posicao na tela
-	                        		float offsetX = random.nextFloat() * camadas[camada].getImageSize();
-	                        		float offsetY = random.nextFloat() * camadas[camada].getImageSize();
-	                        		game.batch.draw( camadas[camada].getTexture(h, j),h * camadas[camada].getImageSize() + offsetX,j * camadas[camada].getImageSize() + offsetY,camadas[camada].getImageSize() / 4,camadas[camada].getImageSize() / 4);
-								}
-	                            
-	                        }
-	      
-	                    }
-	                }
-	            }
+	            
+	            this.renderizaCamadasdoGame();
+	            
 	            game.batch.end();
 	        }
 	    } else 
@@ -211,6 +84,100 @@ public class MainGame implements Screen
 	    }
 	}
 
+	public void renderizaCamadasdoGame()
+	{
+		for (int camada = 0; camada < camadas.length; camada++) 
+        {
+            for (int h = 0; h < camadas[camada].getGridSnap(); h++) 
+            {
+                for (int j = 0; j < camadas[camada].getGridSnap(); j++) 
+                {
+                    if (camadas[camada].getTexture(h, j) != null) 
+                    {
+                    	if(camada != 1)
+                    	{
+                    		game.batch.draw(camadas[camada].getTexture(h, j), h * camadas[camada].getImageSize(), j * camadas[camada].getImageSize(), camadas[camada].getImageSize(), camadas[camada].getImageSize());
+                    	}
+                    	else 
+                    	{//Camada de detalhes do mapa PLACEHOLDER
+                    		
+                    		// Usando uma funcao hash para gerar numeros pseudo-aleatorios
+                    		long seed = (long)h * 2654435761L + (long)j * 2654435789L;
+                    		Random random = new Random(seed);
+                    		//Posicao na tela
+                    		float offsetX = random.nextFloat() * camadas[camada].getImageSize();
+                    		float offsetY = random.nextFloat() * camadas[camada].getImageSize();
+                    		game.batch.draw( camadas[camada].getTexture(h, j),h * camadas[camada].getImageSize() + offsetX,j * camadas[camada].getImageSize() + offsetY,camadas[camada].getImageSize() / 4,camadas[camada].getImageSize() / 4);
+						}
+                        
+                    }
+  
+                }
+            }
+        }
+	}
+	public void generateCamadaTextures() 
+	{
+	    for (int i = 0; i < camadas.length; i++) 
+	    {
+	        camadas[i] = new Camada(20);
+	    }
+	    // Camada de detalhes do mapa PLACEHOLDER
+	    this.generateDetailTextures(camadas[1]);
+
+	    // Gerando chão (tile de grama)
+	    this.generateGroundTextures(camadas[0]);
+
+	    // Gerando areia abaixo do tile quebrável
+	    this.generateSandTextures(camadas[0]);
+	    // Gerando tile quebraveis 
+	    this.generateBreakableTileTextures(camadas[3]);
+	}
+	private void generateDetailTextures(Camada camada) 
+	{
+	    for (int i = 0; i < camada.getGridSnap(); i += 2) 
+	    {
+	        for (int j = 0; j < camada.getGridSnap(); j += 2) 
+	        {
+	            Random random = new Random();
+	            int indexi = random.nextInt(detailTextures.length);
+	            int indexii = random.nextInt(detailTextures[indexi].length);
+	            camada.setTexture(detailTextures[indexi][indexii], i, j);
+	        }
+	    }
+	}
+	private void generateBreakableTileTextures(Camada camada)
+	{
+		for(int i = 1;i<camada.getGridSnap();i+=2)
+		{
+			for(int j = 1;j<camada.getGridSnap();j+=2)
+			{
+				camadas[3].setTexture(new Texture("tile.jpg"), i, j);
+			}
+		}
+	}
+
+	private void generateGroundTextures(Camada camada) 
+	{
+	    for (int i = 0; i < camada.getGridSnap(); i++) 
+	    {
+	        for (int j = 0; j < camada.getGridSnap(); j++) 
+	        {
+	            camada.setTexture(chaoTexture, i, j);
+	        }
+	    }
+	}
+
+	private void generateSandTextures(Camada camada) 
+	{
+	    for (int i = 1; i < camada.getGridSnap(); i += 2) 
+	    {
+	        for (int j = 1; j < camada.getGridSnap(); j += 2) 
+	        {
+	            camada.setTexture(new Texture("chao.png"), i, j);
+	        }
+	    }
+	}
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
