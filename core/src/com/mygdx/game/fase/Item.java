a69c61ba55bce4cd98aee4712e717bba41e9f035
Rhuan Marques <rhuanleonardobm@gmail.com>
2023-11-04 00:51:07 -0300

    Novo Patch: Paredes e Items

--- core/src/com/mygdx/game/fase/Item.java
+++ core/src/com/mygdx/game/fase/Item.java
@@ -1,16 +1,23 @@
 package com.mygdx.game.fase;
 import com.badlogic.gdx.graphics.Texture;
-public class Drop_BombaVerm extends ObjetoDoJogo{
+public class Drop_BombaVerm extends ObjetoDoJogo implements Item{
     private static Texture itemBombaV = new Texture("Drop_BombaVermelha.png");
-
+    private String tipo;
     public static Texture getTexture()
     {
         return itemBombaV;
     }
-    public Drop_BombaVerm(int posX, int posY)
+    public Drop_BombaVerm(int posX, int posY, String tipo)
     {
         this.setPosX(posX);
         this.setPosY(posY);
         this.setTexture(itemBombaV);
+        this.tipo = new String;
     }
+
+    @Override
+    public void recebido(Player player) {
+        player.recebeBombaEspecial(this.tipo);
+    }
+
 }
